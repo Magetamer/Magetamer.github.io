e1a37b6c2a1741a28558e71c821f3b10cbb01ab0
Magetamer <daniel.chahbour@gmail.com>
2023-05-11 19:20:21 -0700

    Final Fix Hopefully

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import React from 'react';
 
 function Square({value, onSquareClick}) {
   return (
@@ -36,7 +37,7 @@ function Board({ xIsNext, squares, onPlay }) {
     }
 
       return (
-    <>
+    <div>
       <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} /> 
@@ -53,7 +54,7 @@ function Board({ xIsNext, squares, onPlay }) {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
         </div>
-    </>
+    </div>
     );
 
     
